69ad7ec4d5618067617e005c151c0b07168d798b
Auxonn <behoven@yandex.ru>
2018-03-12 16:30:12 +0300

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Dog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Dog
+    {
+        private string name;
+        private string breed;
+        private float age;
+
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public float Age { get; set; }
+
+        public Dog()
+        {
+            name = null;
+            breed = null;
+            age = 0;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Кличка: " + name);
+            Console.WriteLine("Порода: " +breed);
+            Console.WriteLine("Возраст: " + age);
+        }
+        public void Input()
+        {
+            Console.Write("Кличка: ");
+            name = Console.ReadLine();
+            Console.Write("Порода: ");
+            breed = Console.ReadLine();
+            Console.Write("Возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Информация о собаке");
+            Dog dg1 = new Dog();
+            dg1.Input();
+            Console.WriteLine();
+            Console.WriteLine("Информация о собаке");
+            dg1.Print();
+            Console.ReadKey();
+        }
+    }
+}
